c15db503d4a6d539cde0b71170fdf63e57ea86d2
Rachel Yang <60298873+Lunar-Flare@users.noreply.github.com>
2021-12-01 11:22:51 -0500

    update

--- Unmasked/unmasked_proj/unmasked_proj/urls.py
+++ Unmasked/unmasked_proj/unmasked_proj/urls.py
@@ -14,17 +14,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+<<<<<<< Updated upstream
 from django.urls import path, include
 from . import views
+=======
+from django.urls import path,include 
+>>>>>>> Stashed changes
 from django.conf import settings
 from django.conf.urls.static import static
+from unmasked_proj import views
 
 urlpatterns = [
+<<<<<<< Updated upstream
     path('admin/', admin.site.urls),
     path('About.html',views.About, name ="About"),
     path('add.html'),
     path('home.html',views.Home, name ='Home'),
     
+=======
+
+    path ('', views.index, name='test'),
+    path ('About.html', views.about, name= 'about'),
+    path ('Add.html', views.add,name="add"),
+    path ('AdminHome.html',views.adminHome, name= 'adminHome'),
+    path ('Contact.html',views.contact, name ='contact'),
+    path ('GeneralQuestions.html',views.genQuestions, name="generalQuestions"),
+    path ('Home.html', views.home, name='home'),
+    path ('Login.html', views.login, name="login"),
+    path ('Logout.html', views.logout, name='logout'),
+    path ('ManageStudents.html', views.manageStudents, name="manageStudents"),
+    path ('Support.html', views.support, name="support"),
+    path ('Tips.html',views.tips,name='tips'),
+>>>>>>> Stashed changes
 ]
 
 urlpatterns+= static(
