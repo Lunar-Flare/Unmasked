24080c1ed58024997c4b83f7e4e434a565c16dcb
Rachel Yang <60298873+Lunar-Flare@users.noreply.github.com>
2021-12-01 07:31:39 -0500

    Updates

--- Unmasked/unmasked_proj/unmasked_proj/urls.py
+++ Unmasked/unmasked_proj/unmasked_proj/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.Home, name ='Home'),
+    path('About.html',views.About, name ="About"),
+    path('add.html'),
+    path('home.html',views.Home, name ='Home'),
+    
 ]
 
 urlpatterns+= static(
---
+++ Unmasked/unmasked_proj/unmasked_proj/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import pyrebase
+
+config ={
+    "apiKey": "AIzaSyAxV8-iToKuLitUmG48EEkIddvq7iYrN2Y",
+    "authDomain": "facial-recongition-38069.firebaseapp.com",
+    "databaseURL": "https://facial-recongition-38069-default-rtdb.firebaseio.com",
+    "projectId": "facial-recongition-38069",
+   "storageBucket": "facial-recongition-38069.appspot.com",
+    "messagingSenderId": "937313859878",
+    "appId": "1:937313859878:web:25d017bad4c1df1255e9e7",
+}
+
+firebase=pyrebase.initialize_app(config)
+authe = firebase.auth()
+database=firebase.database()
+
+def index(request):
+    name = database.child("users").child("GTest").childe("email").get.val()
+    return render(request, 'Home.html')
